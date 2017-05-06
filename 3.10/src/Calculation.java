857c210bcc267a31c428ee6a0bfb33d756237628
Administrator <Administrator@A9294Computer>
2017-05-06 23:31:38 +0800

    Change-Id: Iba031f9a8e8f00909aa91c8037b658c679a7da3f Signed-off-by: Administrator <Administrator@A9294Computer>

---
+++ 3.10/src/Calculation.java
@@ -0,0 +1,11 @@
+public class Calculation {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		boolean a;
+		if (20 < 45)
+			a = true;
+		else
+			a = false;
+		System.out.println(a);
+	}
+}
